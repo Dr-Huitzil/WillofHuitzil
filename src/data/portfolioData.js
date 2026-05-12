4150d55a1219207ed477ee242b4fea57c8a40016
Dr-Huitzil <ialierreyes@gmail.com>
2026-05-12 13:31:13 -0700

    that did not need to be updated

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,7 +1,7 @@
 // src/data/portfolioData.js
 import { esteCosmosDetails } from './projectDetails/esteCosmos';
 import { mindMeltDetails } from './projectDetails/mindMelt';
-import { clinicaDeSaludDetails } from './experienceDetails/clinicaDeSaludDetails';
+import { clinicaDeSaludDetails } from './experienceDetails/clinicaDeSalud';
 import { proActDetails } from './experienceDetails/proActDetails';
 import { mannsFreshDetails } from './experienceDetails/mannsFreshDetails';
 import { starphyreDetails } from './experienceDetails/starphyreDetails';
