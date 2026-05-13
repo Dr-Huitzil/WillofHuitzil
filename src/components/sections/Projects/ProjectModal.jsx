6abfdd7a805d761731732f829b19136c9f4c41a5
Dr_Huitzil <gotenkscoolness300@gmail.com>
2026-05-12 18:55:33 -0700

    scrolling behaviour

--- src/components/sections/Projects/ProjectModal.jsx
+++ src/components/sections/Projects/ProjectModal.jsx
@@ -7,9 +7,17 @@ const ProjectModal = ({ project, onClose }) => {
   const [isExpanded, setIsExpanded] = useState(false);
 
   useEffect(() => {
+    const scrollY = window.scrollY;
     document.body.style.overflow = 'hidden';
+    document.body.style.position = 'fixed';
+    document.body.style.top = `-${scrollY}px`;
+    document.body.style.width = '100%';
     return () => {
       document.body.style.overflow = '';
+      document.body.style.position = '';
+      document.body.style.top = '';
+      document.body.style.width = '';
+      window.scrollTo(0, scrollY);
     };
   }, []);
 
